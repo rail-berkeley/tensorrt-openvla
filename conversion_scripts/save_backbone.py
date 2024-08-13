4e6a9fc19d37aac8c5e59a4196b7d8ba88d5367a
will <verityw@berkeley.edu>
2024-08-13 14:07:10 -0700

    initial commit

---
+++ conversion_scripts/save_backbone.py
@@ -0,0 +1,89 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoModelForVision2Seq, AutoProcessor, AutoTokenizer, AutoConfig
+from transformers.dynamic_module_utils import get_class_from_dynamic_module
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+parser.add_argument('--save-dir', default="./save_dir", type=str)
+parser.add_argument('--test-load', action='store_true')
+
+args = parser.parse_args()
+
+save_dir = args.save_dir
+
+if not args.test_load:
+    # Load OpenVLA
+    print("Saving modules separately")
+    print("Loading VLA")
+    model_name = "Embodied-CoT/ecot-openvla-7b-bridge"
+    vla = AutoModelForVision2Seq.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
+    )
+
+    llm = vla.language_model
+    vision_backbone = vla.vision_backbone
+    projector = vla.projector
+
+    processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer = processor.tokenizer
+    print("Done!")
+
+    # Save tokenizer / LLM
+    print("Saving LLM and tokenizer...")
+    llm.save_pretrained(os.path.join(save_dir, "LLM_backbone")) 
+    tokenizer.save_pretrained(os.path.join(save_dir, "LLM_backbone"))
+
+    # Save vision backbone and projector
+    print("Saving vision backbone and projector...")
+    torch.save(vision_backbone.state_dict(), os.path.join(save_dir, "vision_backbone.pth"))
+    torch.save(projector.state_dict(), os.path.join(save_dir, "projector.pth"))
+else:
+    print("Testing load from new save files")
+
+    # Load LLM via HF AutoModel
+    print("Loading LLM...")
+    llm = AutoModelForCausalLM.from_pretrained(os.path.join(save_dir, "LLM_backbone"))
+    tokenizer = AutoTokenizer.from_pretrained(os.path.join(save_dir, "LLM_backbone"))
+    print("Successful!")
+
+    # Get class for vision backbone using HF utils, then load
+    print("Loading vision backbone...")
+    pretrained_model_name_or_path = "Embodied-CoT/ecot-openvla-7b-bridge"
+    config = AutoConfig.from_pretrained(
+                    pretrained_model_name_or_path,
+                    trust_remote_code=True
+                )
+
+    vision_backbone_class_ref = config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticVisionBackbone")
+    vision_backbone_class = get_class_from_dynamic_module(
+        vision_backbone_class_ref, pretrained_model_name_or_path
+    )
+    vision_backbone = vision_backbone_class(
+                config.use_fused_vision_backbone, config.image_sizes, config.timm_model_ids, config.timm_override_act_layers
+            )
+    vision_backbone.load_state_dict(torch.load(os.path.join(save_dir, "vision_backbone.pth")))
+    print("Successful!")
+
+    # Get class for projector using HF utils, then load
+    print("Loading projector...")
+    proj_class_ref = config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticProjector")
+    proj_class = get_class_from_dynamic_module(
+        proj_class_ref, pretrained_model_name_or_path
+    )
+    projector = proj_class(
+        config.use_fused_vision_backbone,
+        vision_dim=vision_backbone.embed_dim,
+        llm_dim=config.text_config.hidden_size,
+    ).load_state_dict(torch.load(os.path.join(save_dir, "projector.pth")))
+    print("Successful!")
+
+

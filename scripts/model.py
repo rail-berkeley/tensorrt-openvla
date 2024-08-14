6a808d702a60d069d2bde65d100dc6fa614d463d
will <verityw@berkeley.edu>
2024-08-14 16:56:14 -0700

    refactor trtopenvla

--- scripts/model.py
+++ scripts/model.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 import torch
 from transformers import (AutoConfig, AutoProcessor, AutoModelForVision2Seq)
@@ -8,20 +9,32 @@ from transformers.dynamic_module_utils import get_class_from_dynamic_module
 import tensorrt_llm
 from tensorrt_llm.runtime import ModelRunner
 
-import requests
-import argparse
+# TODO: Refactor to directly subclass OpenVLAForActionPrediction
 
 class TRTOpenVLA:
     def __init__(self, save_dir, engine_dir, hf_name, device="cuda"):
         self.engine_dir = engine_dir
         self.save_dir = save_dir
         self.hf_name = hf_name
-
+        self.device = device
+        
+        self.config = AutoConfig.from_pretrained(
+                        self.hf_name,
+                        trust_remote_code=True
+                    )
+        
+        self._setup_action_info()
         self._setup_llm_runner()
-        self.processor = AutoProcessor.from_pretrained(self.hf_name, 
-                                                       trust_remote_code=True)
         self._setup_vision_backbone_and_projector()
 
+        self.end_id = 2 # TODO: Change how to extract EOS token ID
+        self.pad_id = self.config.pad_token_id
+
+    def _setup_action_info(self):
+        self.norm_stats = self.config.norm_stats
+        self.bins = np.linspace(-1, 1, self.config.n_action_bins)
+        self.bin_centers = (self.bins[:-1] + self.bins[1:]) / 2.0
+        self.vocab_size = self.config.text_config.vocab_size - self.config.pad_to_multiple_of
         
     def _setup_llm_runner(self):
         runtime_rank = tensorrt_llm.mpi_rank()
@@ -32,17 +45,15 @@ class TRTOpenVLA:
         self.llm_runner = ModelRunner.from_dir(**runner_kwargs)
 
     def _setup_vision_backbone_and_projector(self):
-        config = AutoConfig.from_pretrained(
-                        self.hf_name,
-                        trust_remote_code=True
-                    )
 
-        vision_backbone_class_ref = config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticVisionBackbone")
+        vision_backbone_class_ref = self.config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", 
+                                                                                                  "PrismaticVisionBackbone")
         vision_backbone_class = get_class_from_dynamic_module(
             vision_backbone_class_ref, self.hf_name
         )
         self.vision_backbone = vision_backbone_class(
-                    config.use_fused_vision_backbone, config.image_sizes, config.timm_model_ids, config.timm_override_act_layers
+                    self.config.use_fused_vision_backbone, self.config.image_sizes, 
+                    self.config.timm_model_ids, self.config.timm_override_act_layers
                 )
         self.vision_backbone.load_state_dict(torch.load(os.path.join(self.save_dir, "vision_backbone.pth"), map_location=self.device))
         self.vision_backbone.featurizer.to(self.device, torch.bfloat16)
@@ -50,65 +61,54 @@ class TRTOpenVLA:
 
         # Get class for projector using HF utils, then load
         print("Loading projector...")
-        proj_class_ref = config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticProjector")
+        proj_class_ref = self.config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticProjector")
         proj_class = get_class_from_dynamic_module(
             proj_class_ref, self.hf_name
         )
         self.projector = proj_class(
-            config.use_fused_vision_backbone,
+            self.config.use_fused_vision_backbone,
             vision_dim=self.vision_backbone.embed_dim,
-            llm_dim=config.text_config.hidden_size,
+            llm_dim=self.config.text_config.hidden_size,
         )
         self.projector.load_state_dict(torch.load(os.path.join(self.save_dir, "projector.pth"), 
                                              map_location=self.device))
         self.projector.to(self.device, torch.bfloat16)
 
-    def parse_input(self,
-                    input_text,
-                    add_special_tokens=True,
-                    max_input_length=512
-                   ):
-        tokenizer = self.processor.tokenizer
-        batch_input_ids = []
-        for curr_text in input_text:
-            input_ids = tokenizer.encode(curr_text,
-                                         add_special_tokens=add_special_tokens,
-                                         truncation=True,
-                                         max_length=max_input_length)
-            batch_input_ids.append(input_ids)
-        
+    def parse_input(self, input_ids):
         base_vocab_size = 32064
-        for i in range(len(batch_input_ids)):
-            batch_input_ids[i] = batch_input_ids[i][:1] + \
-                list(range(base_vocab_size, base_vocab_size + 256)) + \
-                batch_input_ids[i][1:]
+        batch_size = input_ids.shape[0]
+        
+        virt_tokens = torch.tensor([list(range(base_vocab_size, base_vocab_size + 256))] * batch_size, dtype=torch.int32)
+        batch_input_ids = torch.cat([input_ids[:, :1], virt_tokens, input_ids[:, 1:]], dim=1)
 
-        batch_input_ids = [
-            torch.tensor(x, dtype=torch.int32) for x in batch_input_ids
-        ]
+        # for i in range(len(batch_input_ids)):
+        #     batch_input_ids[i] = batch_input_ids[i][:1] + \
+        #         list(range(base_vocab_size, base_vocab_size + 256)) + \
+        #         batch_input_ids[i][1:]
+
+        # batch_input_ids = [
+        #     torch.tensor(x, dtype=torch.int32) for x in batch_input_ids
+        # ]
         return batch_input_ids
     
     def generate(self, 
-                 prompt, 
-                 image,
+                 inputs,
                  max_new_tokens=400,
                  do_sample=False,
                  random_seed=0,
                  **kwargs):
-        pixel_values = self.processor("", image)["pixel_values"].to(self.device, dtype=torch.bfloat16)
+        
+        pixel_values = inputs["pixel_values"].to(self.device, dtype=torch.bfloat16)
         patch_features = self.vision_backbone(pixel_values)
         projected_patch_embeddings = self.projector(patch_features)
-        batch_input_ids = self.parse_input([prompt])
+        batch_input_ids = self.parse_input(inputs["input_ids"])
 
-        end_id = self.processor.tokenizer.eos_token_id
-        pad_id = self.processor.tokenizer.pad_token_id
-
-        outputs = self.runner.generate(
+        outputs = self.llm_runner.generate(
             batch_input_ids=batch_input_ids,
             encoder_input_ids=None,
             max_new_tokens=max_new_tokens,
-            end_id=end_id,
-            pad_id=pad_id,
+            end_id=self.end_id,
+            pad_id=self.pad_id,
             random_seed=random_seed,
             do_sample=do_sample,
             prompt_table=projected_patch_embeddings,
@@ -118,23 +118,65 @@ class TRTOpenVLA:
         torch.cuda.synchronize()
         return outputs
     
-    def get_action(self, instruction, image, **kwargs):
-        prompt = f"A chat between a curious user and an artificial intelligence assistant." +\
-            f" The assistant gives helpful, detailed, and polite answers to the user's questions." + \
-            f" USER: What action should the robot take to {instruction.lower()}? ASSISTANT: TASK:"
-        
-        outputs = self.generate(prompt, 
-                                image,
-                                max_new_tokens=400,
-                                do_sample=False,
-                                random_seed=0,
-                                return_dict=True
+    def get_action(self, inputs, unnorm_key=None, do_sample=False, random_seed=0, **kwargs):
+        outputs = self.generate(inputs,
+                                max_new_tokens=512,
+                                return_dict=True,
+                                do_sample=do_sample,
+                                random_seed=random_seed,
                                 **kwargs)
         
-        output_ids = outputs['output_ids']
-        output_text = self.processor.tokenizer.decode(output_ids.reshape(-1), skip_special_tokens=True)
-        raise NotImplementedError
+        generated_ids = outputs['output_ids']
+        
+        # TRT-LLM Llama generates EOS tokens until max_new_tokens is saturated, so need to remove all EOS tokens
+        not_eos_mask = generated_ids != self.end_id
+        generated_ids = generated_ids[not_eos_mask]
 
+        #  Extract predicted action tokens and translate into (normalized) continuous actions
+        predicted_action_token_ids = generated_ids[-self.get_action_dim(unnorm_key):].cpu().numpy()
+        discretized_actions = self.vocab_size - predicted_action_token_ids
+        discretized_actions = np.clip(discretized_actions - 1, a_min=0, a_max=self.bin_centers.shape[0] - 1)
+        normalized_actions = self.bin_centers[discretized_actions]
 
+        # Unnormalize actions
+        action_norm_stats = self.get_action_stats(unnorm_key)
+        mask = action_norm_stats.get("mask", np.ones_like(action_norm_stats["q01"], dtype=bool))
+        action_high, action_low = np.array(action_norm_stats["q99"]), np.array(action_norm_stats["q01"])
+        actions = np.where(
+            mask,
+            0.5 * (normalized_actions + 1) * (action_high - action_low) + action_low,
+            normalized_actions,
+        )
+
+        return actions, generated_ids
+
+    @staticmethod
+    def _check_unnorm_key(norm_stats, unnorm_key):
+        if unnorm_key is None and len(norm_stats) != 1:
+            raise ValueError(
+                f"Your model was trained on more than one dataset. "
+                f"Please pass a `unnorm_key` from the following options to choose the statistics used for "
+                f"de-normalizing actions: {norm_stats.keys()}"
+            )
+
+        # If None, grab the (singular) dataset in `norm_stats` to use as `unnorm_key`
+        unnorm_key = unnorm_key if unnorm_key is not None else next(iter(norm_stats.keys()))
+        if unnorm_key not in norm_stats:
+            raise ValueError(
+                f"The `unnorm_key` you chose ({unnorm_key = }) is not in the available statistics. "
+                f"Please choose from: {norm_stats.keys()}"
+            )
+
+        return unnorm_key
+
+    def get_action_dim(self, unnorm_key=None):
+        """Get the dimensionality of the policy's action space."""
+        unnorm_key = self._check_unnorm_key(self.norm_stats, unnorm_key)
+        return len(self.norm_stats[unnorm_key]["action"]["q01"])
+
+    def get_action_stats(self, unnorm_key=None) :
+        """Get all the logged statistics for the given dataset."""
+        unnorm_key = self._check_unnorm_key(self.norm_stats, unnorm_key)
+        return self.norm_stats[unnorm_key]["action"]
 
         
--- scripts/openvla_run.py
+++ scripts/openvla_run.py
@@ -36,6 +36,8 @@ import argparse
 import textwrap
 import enum
 
+from model import TRTOpenVLA
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--save-dir', default="./save_dir", type=str)
@@ -47,55 +49,8 @@ device = "cuda"
 engine_dir = os.path.join(args.ckpts_dir, "openvla_engine")
 save_dir = args.save_dir
 
-print("Loading compiled LLM")
-runtime_rank = tensorrt_llm.mpi_rank()
-runner_kwargs = dict(
-        engine_dir=engine_dir,
-        rank=runtime_rank,
-    )
-runner = ModelRunner.from_dir(**runner_kwargs)
-print("Done!")
-
-print("Loading tokenizer...")
-path_to_converted_ckpt = "Embodied-CoT/ecot-openvla-7b-bridge"
-processor = AutoProcessor.from_pretrained(path_to_converted_ckpt, trust_remote_code=True)
-tokenizer = processor.tokenizer
-pad_id = tokenizer.pad_token_id
-end_id = tokenizer.eos_token_id
-print("Done!")
-
-print("Loading vision backbone...")
-pretrained_model_name_or_path = "Embodied-CoT/ecot-openvla-7b-bridge"
-config = AutoConfig.from_pretrained(
-                pretrained_model_name_or_path,
-                trust_remote_code=True
-            )
-
-vision_backbone_class_ref = config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticVisionBackbone")
-vision_backbone_class = get_class_from_dynamic_module(
-    vision_backbone_class_ref, pretrained_model_name_or_path
-)
-vision_backbone = vision_backbone_class(
-            config.use_fused_vision_backbone, config.image_sizes, config.timm_model_ids, config.timm_override_act_layers
-        )
-vision_backbone.load_state_dict(torch.load(os.path.join(save_dir, "vision_backbone.pth"), map_location=device))
-vision_backbone.featurizer.to(device, torch.bfloat16)
-print("Successful!")
-
-# Get class for projector using HF utils, then load
-print("Loading projector...")
-proj_class_ref = config.auto_map[AutoModelForVision2Seq.__name__].replace("OpenVLAForActionPrediction", "PrismaticProjector")
-proj_class = get_class_from_dynamic_module(
-    proj_class_ref, pretrained_model_name_or_path
-)
-projector = proj_class(
-    config.use_fused_vision_backbone,
-    vision_dim=vision_backbone.embed_dim,
-    llm_dim=config.text_config.hidden_size,
-)
-projector.load_state_dict(torch.load(os.path.join(save_dir, "projector.pth"), map_location=device))
-projector.to(device, torch.bfloat16)
-print("Successful!")
+processor = AutoProcessor.from_pretrained( "Embodied-CoT/ecot-openvla-7b-bridge", trust_remote_code=True)
+vla = TRTOpenVLA(save_dir, engine_dir, "Embodied-CoT/ecot-openvla-7b-bridge", device=device)
 
 print("Getting image...")
 url = 'https://raw.githubusercontent.com/MichalZawalski/embodied-CoT/main/test_obs.png'
@@ -103,65 +58,17 @@ page = requests.get(url)
 image = Image.open(BytesIO(page.content))
 print("Done!")
 
-def parse_input(tokenizer,
-                input_text,
-                prompt_template=None,
-                add_special_tokens=True,
-                max_input_length=512,
-                pad_id=None):
-    if pad_id is None:
-        pad_id = tokenizer.pad_token_id
-
-    batch_input_ids = []
-    for curr_text in input_text:
-        if prompt_template is not None:
-            curr_text = prompt_template.format(input_text=curr_text)
-        input_ids = tokenizer.encode(curr_text,
-                                        add_special_tokens=add_special_tokens,
-                                        truncation=True,
-                                        max_length=max_input_length)
-        batch_input_ids.append(input_ids)
-    
-    base_vocab_size = 32064
-    for i in range(len(batch_input_ids)):
-        batch_input_ids[i] = batch_input_ids[i][:1] + \
-            list(range(base_vocab_size, base_vocab_size + 256)) + \
-            batch_input_ids[i][1:]
-
-    batch_input_ids = [
-        torch.tensor(x, dtype=torch.int32) for x in batch_input_ids
-    ]
-    return batch_input_ids
-
 print("Running inference...")
 start = time.time()
 
 prompt = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: What action should the robot take to place the watermelon on the towel? ASSISTANT: TASK:"
-batch_input_ids = parse_input(tokenizer, [prompt])
+inputs = processor(prompt, image)
 
 with torch.no_grad():
-    pixel_values = processor("", image)["pixel_values"].to(device, dtype=torch.bfloat16)
-    patch_features = vision_backbone(pixel_values)
-    projected_patch_embeddings = projector(patch_features)
-
-    outputs = runner.generate(
-        batch_input_ids=batch_input_ids,
-        encoder_input_ids=None,
-        max_new_tokens=400,
-        end_id=end_id,
-        pad_id=pad_id,
-        early_stopping=1,
-        do_sample=False,
-        random_seed=0,
-        prompt_table=projected_patch_embeddings,
-        prompt_vocab_size=256,
-        output_sequence_lengths=True,
-        return_dict=True,)
+    action, generated_ids = vla.get_action(inputs, unnorm_key="bridge_orig") 
     torch.cuda.synchronize()
 
 print(f"Done! Time elapsed: {time.time() - start}.")
 
-output_ids = outputs['output_ids']
-output_text = tokenizer.decode(output_ids.reshape(-1), skip_special_tokens=True)
-
-print(output_text)
+print(action)
+print(processor.decode(generated_ids))
